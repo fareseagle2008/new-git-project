2a3349fd727269c1582ea514cf78717786eb00c3
fareseagle2008 <fareseagle2008@hotmail.com>
2018-02-13 15:34:32 +0200

    Update models.py

--- forums/models.py
+++ forums/models.py
@@ -4,8 +4,6 @@ class Member:
 		self.age = age
 
 class Post:
-	def __init__(self, title, body, author, creation_date):
+	def __init__(self, title, body):
 		self.title = title
 		self.body = body
-		self.author = author
-		self.creation_date = creation_date
