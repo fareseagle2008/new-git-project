8d7b80dd8a33541cc54a177f69ab7783215c9397
fareseagle2008 <fareseagle2008@hotmail.com>
2018-02-13 14:50:11 +0200

    Add files via upload

---
+++ forums/main.py
@@ -0,0 +1,13 @@
+import models
+from datetime import date
+
+member1 = models.Member("Fares AbuAmin", 31)
+member2 = models.Member("Lama Kheir", 30)
+
+post1 = models.Post('HEDONIST ROOTS', 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,', 'Fares AbuAmin', date.today())
+post2 = models.Post("LOREM IPSUM IN THE NEWS", "Five centuries later Lorem Ipsum experienced a surge in popularity with the release of Letraset's dry-transfer sheets.", "Lama Kheir", date.today())
+post3 = models.Post("FROM PRINT TO DESKTOP TO WEB", "Aldus Corporation, which later merged with Adobe Systems, helped bring Lorem Ipsum into the information age with its 1985 flagship software Aldus PageMaker. ", "Fares AbuAmin", date.today())
+
+print(post1.body)
+print(post2.creation_date)
+print(member1.name)
---
+++ forums/models.py
@@ -0,0 +1,11 @@
+class Member:
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+
+class Post:
+	def __init__(self, title, body, author, creation_date):
+		self.title = title
+		self.body = body
+		self.author = author
+		self.creation_date = creation_date
